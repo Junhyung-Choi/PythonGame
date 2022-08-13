8ebb1bdd7881084c6ea59feb878af797902bf519
Junhyung Choi <junhyung9571@naver.com>
2022-08-13 19:42:16 +0900

    Refactor: Main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import setting
 import menu
 import story
 import meeting
-
 #############################################
 #                                            
 #    stage  0 : 메뉴 씬                      
@@ -14,43 +13,40 @@ import meeting
 #                                            
 #############################################
 
-# 파이게임 
-pygame.init()
-
-# 게임 전체 루프
-while setting.running:
+def loop():
     # 이벤트 처리
-    for event in pygame.event.get():
-        # 모든 스테이지에서 공통적으로 사용되는 이벤트
-        if event.type == pygame.QUIT:
-            setting.running = False
-        
-        if event.type == pygame.KEYDOWN: #키보드의 키가 눌러졌을 경우
-            # 개발자 테스트용 스테이지 이동 키
-            if event.key == pygame.K_MINUS: 
-                setting.stage -= 1
-                print("현재 스테이지 : " + str(setting.stage))
-
-            if event.key == pygame.K_EQUALS:
-                setting.stage += 1
-                print("현재 스테이지 : " + str(setting.stage))
+    event()
+    # 각 스테이지 렌더링
+    render()
 
 
-        ####################################################
+def event():
+    while setting.running:
+        # 이벤트 처리
+        for event in pygame.event.get():
+            # 모든 스테이지에서 공통적으로 사용되는 이벤트
+            if event.type == pygame.QUIT:
+                setting.running = False
+            if event.type == pygame.KEYDOWN: #키보드의 키가 눌러졌을 경우
+                # 개발자 테스트용 스테이지 이동 키
+                if event.key == pygame.K_MINUS: 
+                    setting.stage -= 1
+                    print("현재 스테이지 : " + str(setting.stage))
+                if event.key == pygame.K_EQUALS:
+                    setting.stage += 1
+                    print("현재 스테이지 : " + str(setting.stage))
 
-        # 스테이지별 이벤트
-        if setting.stage == 0: # 시작 화면일때
-            menu.process_event(event=event)
+            # 스테이지별 이벤트
+            if setting.stage == 0: # 시작 화면일때
+                menu.process_event(event=event)
 
-        elif setting.stage == 1: # 첫 번째 stage 이벤트
-            story.process_event(event=event)
+            elif setting.stage == 1: # 첫 번째 stage 이벤트
+                story.process_event(event=event)
 
-        elif setting.stage == 2: # 두 번째 stage 이벤트
-            meeting.process_event(event=event)
+            elif setting.stage == 2: # 두 번째 stage 이벤트
+                meeting.process_event(event=event)
 
-        ####################################################
-
-    # 각 스테이지별 따로 설졍해줘야 하는 요소들
+def render():
     if setting.stage == 0:
         menu.render()
     elif setting.stage == 1:
@@ -64,4 +60,10 @@ while setting.running:
 
     pygame.display.update()
 
+
+# 파이게임 초기화
+pygame.init()
+# 게임 전체 루프
+loop()
+# 파이게임 종료
 pygame.quit()
--- setting.py
+++ setting.py
@@ -1,17 +1,18 @@
 import pygame
-import story
-import menu
-import meeting
 
-# 초기 변수 설정
+# 스크린 사이즈 설정
 screen_width = 480
 screen_height = 640
 screen = pygame.display.set_mode((screen_width, screen_height))
+
+# 현재 씬 
+running = True
+stage = 0
+
+# 윈도우 설정
 pygame.display.set_caption("Python Game")
 
+# 이미지 에셋 업로드
 start_background = pygame.image.load("img/start.png")
 background = pygame.image.load("img/background.png")
 
-# 이벤트 루프 변수
-running = True
-stage = 0
