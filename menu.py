bfabbc1883bb5fb6ab73bf52455342ace22ccd41
Junhyung Choi <junhyung9571@naver.com>
2022-08-15 20:20:38 +0900

    Fix: image size

--- menu.py
+++ menu.py
@@ -18,20 +18,29 @@ def process_event(event):
 
 def render():
     setting.screen.blit(setting.main_background, (0, 0))
+    
     frame_number = 36
     maxHeight = setting.screen.get_height()
     maxWidth = setting.screen.get_width()
     
     mouseX, mouseY = pygame.mouse.get_pos()
-    setting.screen.blit(setting.eyeBall,(mouseX,mouseY))
+    eyeX = int(remap(mouseX,0,maxWidth,462,462+200))
+    eyeY = int(remap(mouseY,0,maxHeight,527,627))
+    setting.screen.blit(setting.eyeBall,(eyeX,eyeY))
+    setting.screen.blit(setting.eyeBall,(eyeX+435,eyeY))
     
     if(mouseY < maxHeight / 2):
         setting.screen.blit(setting.eyes[17],(0,0))
     else:
-        fidx = int(remap(mouseX,0,maxWidth,0,frame_number))
-        setting.screen.blit(setting.eyes[fidx],(0,0))
+        if mouseX < 300:
+            setting.screen.blit(setting.eyes[0],(0,0))
+        elif mouseX > 1300:
+            setting.screen.blit(setting.eyes[35],(0,0))
+        else:
+            fidx = int(remap(mouseX,300,1300,0,frame_number))
+            setting.screen.blit(setting.eyes[fidx],(0,0))
     
-    setting.screen.blit(setting.main_title, (0,0))
-    setting.screen.blit(setting.main_table, (0,0))
-    setting.screen.blit(setting.menu_button,(0,0))
-    setting.screen.blit(setting.menu_button,(100,0))
+    setting.screen.blit(setting.main_title, (176,54))
+    setting.screen.blit(setting.main_table, (-40,899))
+    setting.screen.blit(setting.menu_button,(377,974))
+    setting.screen.blit(setting.menu_button,(1087,974))
--- setting.py
+++ setting.py
@@ -2,8 +2,8 @@ from lib2to3.pygram import python_grammar
 import pygame
 
 # 스크린 사이즈 설정
-screen_width = 1920
-screen_height = 1080
+screen_width = 1600
+screen_height = 1200
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 # 현재 씬 
@@ -23,7 +23,18 @@ for idx in range(36):
 eyeBall = pygame.image.load("img/eyeBall.png")
 
 main_table = pygame.image.load("img/Main_Table.png")
+main_table = pygame.transform.scale(main_table,(1681,312))
+
 main_title = pygame.image.load("img/Main_Title.png")
+main_title = pygame.transform.scale(main_title,(1249,404))
+
+
 main_background = pygame.image.load("img/MainBG.png")
+
+
 menu_button_click = pygame.image.load("img/Button_click.png")
+menu_button_click = pygame.transform.scale(menu_button_click,(136,118))
+
+
 menu_button = pygame.image.load("img/Button_normal.png")
+menu_button = pygame.transform.scale(menu_button,(136,118))
