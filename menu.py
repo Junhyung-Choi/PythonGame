1e322e7eefc254e4f8d9c5f977a011e5c8d99de3
Junhyung Choi <junhyung9571@naver.com>
2022-08-22 17:03:24 +0900

    Fix Size

--- menu.py
+++ menu.py
@@ -1,4 +1,5 @@
 from turtle import Screen
+from wsgiref.util import setup_testing_defaults
 import pygame
 import setting
 
@@ -15,6 +16,13 @@ def process_event(event):
             print("패치노트")
     if event.type == pygame.MOUSEMOTION:
         print(pygame.mouse.get_pos())
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        mouseX, mouseY = pygame.mouse.get_pos()
+        if(180 < mouseX < 260 and 480 < mouseY < 540):
+            print("Next Scene")
+            setting.stage += 1
+        if(540 < mouseX < 620 and 480 < mouseY < 540):
+            print("GAME OVER")
 
 def render():
     setting.screen.blit(setting.main_background, (0, 0))
@@ -24,23 +32,32 @@ def render():
     maxWidth = setting.screen.get_width()
     
     mouseX, mouseY = pygame.mouse.get_pos()
-    eyeX = int(remap(mouseX,0,maxWidth,462,462+200))
-    eyeY = int(remap(mouseY,0,maxHeight,527,627))
+    eyeX = int(remap(mouseX,0,maxWidth,230,350))
+    eyeY = int(remap(mouseY,0,maxHeight,280,320))
     setting.screen.blit(setting.eyeBall,(eyeX,eyeY))
-    setting.screen.blit(setting.eyeBall,(eyeX+435,eyeY))
+    setting.screen.blit(setting.eyeBall,(eyeX+200,eyeY))
     
     if(mouseY < maxHeight / 2):
         setting.screen.blit(setting.eyes[17],(0,0))
     else:
-        if mouseX < 300:
+        if mouseX < 170:
             setting.screen.blit(setting.eyes[0],(0,0))
-        elif mouseX > 1300:
+        elif mouseX > 630:
             setting.screen.blit(setting.eyes[35],(0,0))
         else:
-            fidx = int(remap(mouseX,300,1300,0,frame_number))
+            fidx = int(remap(mouseX,170,630,0,frame_number-1))
             setting.screen.blit(setting.eyes[fidx],(0,0))
     
-    setting.screen.blit(setting.main_title, (176,54))
-    setting.screen.blit(setting.main_table, (-40,899))
-    setting.screen.blit(setting.menu_button,(377,974))
-    setting.screen.blit(setting.menu_button,(1087,974))
+    setting.screen.blit(setting.main_title, (176/2,54/2))
+    setting.screen.blit(setting.main_table, (-40/2,899/2))
+
+    if(180 < mouseX < 260 and 480 < mouseY < 540):
+        setting.screen.blit(setting.menu_button_click,(377/2,974/2))
+    else:
+        setting.screen.blit(setting.menu_button,(377/2,974/2))
+
+    if(540 < mouseX < 620 and 480 < mouseY < 540):
+        setting.screen.blit(setting.menu_button_click,(1087/2,974/2))
+    else:
+        setting.screen.blit(setting.menu_button,(1087/2,974/2))
+        
