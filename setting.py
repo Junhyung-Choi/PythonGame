bec020d469b8f61b48c5ca09ed31ea58ed3606ca
peanutyumyum <jrimit@gmail.com>
2022-08-26 14:57:38 +0900

    fix: some commit stuff

--- setting.py
+++ setting.py
@@ -11,13 +11,6 @@ screen_width = 800
 screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 
-character = pygame.image.load("img/dot.png")
-character_size = character.get_rect().size #캐릭터 이미지 사이즈 구하기
-character_width = character_size[0] #캐릭터 가로 크기
-character_height = character_size[1] #캐릭터 세로 크기
-#캐릭터의 기준 좌표를 캐릭터의 왼쪽 상단으로 둔다.
-character_x_pos = (screen_width / 2) - (character_width / 2) #화면 가로 절반의 중간에 위치. 좌우로 움직이는 변수
-character_y_pos = screen_height - character_height #이미지가 화면 세로의 가장 아래 위치
 
 # 현재 씬 
 running = True
