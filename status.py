f5acdd1c6600fb8800bd29ba02ce23d36352d3e7
Junhyung Choi <junhyung9571@naver.com>
2022-09-07 20:51:04 +0900

    Feat: Class GameStatus / Questions

--- status.py
+++ status.py
@@ -3,6 +3,11 @@ import random
 class GameStatus:
     def __init__(self, scene_name):
         self.scene_name = scene_name
+        self.mode = 1
+        self.left_time = 1
+        self.current_questions = []
+        self.current_question = []
+        self.__root_question__ = []
         self.point = 0
         self.questions = self.__make_questions__()
     
@@ -14,31 +19,32 @@ class GameStatus:
             questions.append(Question("New Question_" + str(_)))
 
         return questions
-
-class QuestionBundle:
-    def __init__(self, parent, child=None, questions):
-        self.parent = parent
-        self.child = child
-        self.questions = questions
+    
+    def etc_button_clicked(self, button_type):
+        if(button_type == "pause"):
+            pass
+        elif (button_type == "time"):
+            pass
+        elif (button_type == "goback"):
+            pass
+    
+    def speech_button_clicked(self, index):
+        if(index == 1):
+            pass
+        elif (index == 2):
+            pass
+        elif (index == 3):
+            pass
+        elif (index == 4):
+            pass
 
 class Question:
-    def __init__(self, sentence, child=None, bundle):
-        self.sentence = sentence
-        self.score = random.randint(-3, 3)
-        self.child = child
-        self.bundle = bundle
-
-    def select(self):
-        gs = self.child        
+    def __init__(self):
+        self.point = 0
+        self.sentence = "Question Sentence"
+        self.index = 0
+        self.child_questions = []
 
 
 if __name__ == "__main__":
-    q = Question("New Question")
-    print(q.answer)
-    print(q.sentence)
-
-    gs = GameStatus("New Scene")
-    print(gs.scene_name)
-    print(gs.point)
-    for q in gs.questions:
-        print(q.answer)
+    print("status.py called")
