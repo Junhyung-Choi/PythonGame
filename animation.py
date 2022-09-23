dd47ba2c4ced3ad109c27b785f1e8e7b37dc4618
Junhyung Choi <junhyung9571@naver.com>
2022-09-23 18:06:29 +0900

    Feat: Add Animator Enumerator

--- animation.py
+++ animation.py
@@ -1,3 +1,4 @@
+from enum import Enum
 import pygame
 
 class Animation():
@@ -11,9 +12,34 @@ class Animation():
             img = pygame.transform.scale(img, (800, 600))
             self.imgs.append(img)
         self.now_img = self.imgs[0]
+        self.loop = True
+        self.isPlayed = False
 
     def update(self):
-        self.index += 1
-        if self.index >= len(self.imgs):
-            self.index = 0
-        self.now_img = self.imgs[self.index]
+        # 루프를 돌아야 하거나, 한번 끝까지 재생되었거나
+        if(self.loop or not self.isPlayed):
+            self.index += 1
+            if self.index >= len(self.imgs):
+                self.isPlayed = True
+                self.index = 0
+            self.now_img = self.imgs[self.index]
+
+class Animator():
+    def __init__(self):
+        self.current_animation = None
+        self.animations = {}
+        self.state = AnimatiorState.PLAY
+    
+    def translate(self, key):
+        self.state = AnimatiorState.TRANS
+        self.current_animation = self.animations[key]
+    
+    def add_animation(self, key, ani):
+        self.animations[key] = ani
+    
+
+class AnimatiorState(Enum):
+    PLAY = 1
+    TRANS = 2
+    STOP = 3
+
