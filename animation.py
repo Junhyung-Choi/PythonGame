b6e9839fda02c7067ae37d519c5ab09f3a798546
Junhyung Choi <junhyung9571@naver.com>
2022-09-30 11:54:11 +0900

    Feat: constructing animator class

--- animation.py
+++ animation.py
@@ -15,6 +15,9 @@ class Animation():
         self.loop = True
         self.isPlayed = False
         self.isArmOnTable = True
+    
+    def init():
+        print("initing")
 
     def update(self):
         # 루프를 돌아야 하거나, 한번 끝까지 재생되었거나
@@ -30,14 +33,41 @@ class Animator():
         self.current_animation = None
         self.animations = {}
         self.state = AnimatorState.PLAY
-        self.nextanimation = None
+        self.next_animation = None
     
     def update(self):
-        # 트랜스 상태일때는 애니메이션이 끝났는지를 확인해야함
-        # current_animation이 loop = True 라면 이걸 False로 변경 (이건 translate에서 변경해도 될듯)
-        # current_animation의 now_img가 마지막 img일때 current_animation 등을 진행
-        # 만일 current_animation의 상태가 팔이 전환되는 상황이 필요하다면 이를 전환 할 수 있어야 함
+        """
+        애니메이션의 다음
+
+        if state == TRANS:
+            if 애니메이션이 종료 됨:
+                if 팔 교체해야함 :
+                    팔 교체에 맞는 애니메이션으로 전환
+                    다음 애니메이션 갱신하지 않음
+                    여전히 trans 상태로 둠
+                else (교체 안해도 됨):
+                    다음 애니메이션 pop 후 현재 애니메이션으로 전환
+                    state를 trans 에서 play로 전환
+            else (종료되지 않음):
+                current_animation의 update 함수 호출함.
+        elif state == PLAY
+
+            저쩌구
+        elif state == STOP:
+            그쩌구
+        """
         if(self.state == AnimatorState.TRANS):
+            if(self.current_animation.isPlayed):
+                # if current_animation.isArmOnTable != self.next_animation.isArmOnTable: 관련 함수 작성
+                # else:
+                pass
+            else:
+                self.current_animation = self.next_animation
+                self.current_animation.init()
+                self.state = AnimatorState.PLAY
+                self.next_animation = None
+            self.current_animation.update()
+        else:
             self.current_animation.update()
         if(self.state == AnimatorState.PLAY):
             pass
@@ -46,12 +76,28 @@ class Animator():
         pass
     
     def render(self):
+        """
+        Return:
+            현재 Animator가 출력해야 하는 이미지를 리턴함
+        """
         return self.current_animation.now_img
 
-    
     def translate(self, key):
-        self.state = AnimatorState.TRANS
-        self.current_animation = self.animations[key]
+        """
+        ### 애니메이션을 전환하는 함수\n
+        한번 애니메이션이 전환 중이라면 다음 전환은 입력받지 않음. (디버그 라인 출력)\n
+        다음 애니메이션에 `key`로 입력받은 애니메이션을 등록해둔 후 state를 변경\n
+        current_animation이 계속해서 재생되지 않도록 loop 또한 변경함
+        """
+        # 만일 전환중인 상황이라면
+        if(self.state != AnimatorState.TRANS):
+            print("You cannot translate animation now.")
+            print("This animator is currently tranlsating animation")
+        # 만일 전환중인 상황이 아니라면 전환
+        else:
+            self.next_animation = self.animations[key]
+            self.state = AnimatorState.TRANS
+            self.current_animation.loop = False
     
     def add_animation(self, key, ani):
         self.animations[key] = ani
