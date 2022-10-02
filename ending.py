f0d20663609cccfa8201fb52135d61c32fff91f4
Jaeho Yang <ho4121e@gmail.com>
2022-10-02 23:48:22 +0900

    Chore: 엔딩씬 점수에 따라 다르게 출력되도록 설정. ending.py 리팩토링

--- ending.py
+++ ending.py
@@ -1,22 +1,50 @@
 import pygame
 import setting
-from button import *
-from status import *
+import animation
+import time
 
-global alpha 
-alpha = 400
+class Ending():
+    def __init__(self):
+        self.alpha = 400
+        self.common_imgs = animation.Animation("img/meeting/middle/", 2)
+        self.separate_img = None
+        self.separate_back_img = None
+        self.is_loaded_common_img = False
+        self.is_common_imgs_running = True
+        self.current_scene_number = 0
+        self.start_t = time.time()
 
-def render():
-    global alpha
-    alpha -= 3
+    def load_separate_img(self, kind):
+        self.separate_img = pygame.image.load("img/ending/" + kind + ".png")
+        self.separate_img = pygame.transform.scale(self.separate_img, (800, 600))
+        self.separate_back_img = pygame.image.load("img/ending/back.png")
+        self.separate_back_img = pygame.transform.scale(self.separate_back_img, (800, 600))
 
-    if alpha < -50:
-        setting.stage = 0
+    def render(self, kind):
+        if not self.is_loaded_common_img:
+            self.load_separate_img(kind)
+            self.is_loaded_common_img = True
 
-    back = pygame.image.load("img/ending/back.png")
-    back = pygame.transform.scale(back, (800, 600))
-    end = pygame.image.load("img/ending/end.png")
-    end = pygame.transform.scale(end, (800, 600))
-    end.set_alpha(alpha)
-    screen.blit(back, (0, 0))
-    screen.blit(end, (0, 0))
+        setting.screen.blit(self.common_imgs.now_img, (0, 0))
+
+        if self.is_common_imgs_running:
+            currnet_t = time.time()
+            if self.current_scene_number > 2:
+                if self.start_t + 2 <= currnet_t:
+                    self.is_common_imgs_running = False
+
+            elif self.start_t + 2 <= currnet_t:
+                self.start_t = time.time()
+                self.common_imgs.update()
+                self.current_scene_number += 1
+
+        else:
+            self.alpha -= 3
+            
+            if self.alpha < -50:
+                setting.stage = 0
+
+            self.separate_img.set_alpha(self.alpha)
+            
+            setting.screen.blit(self.separate_back_img, (0, 0))
+            setting.screen.blit(self.separate_img, (0, 0))
