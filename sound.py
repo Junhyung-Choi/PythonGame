df140597a21abc6e6f61c2c6ec027755e1f4b6cf
Jaeho Yang <ho4121e@gmail.com>
2022-10-03 14:11:52 +0900

    Feat: 스토리 씬 소리 추가

--- sound.py
+++ sound.py
@@ -1,11 +1,31 @@
 import pygame
 
 class Sound():
-    def __init__(self):
-        self.sound = None
-
-    def load_sound(self, path):
+    def __init__(self, path):
         self.sound = pygame.mixer.Sound(path)
 
     def play(self):
         self.sound.play()
+
+class SceneSound(Sound):
+    def __init__(self, path, sound_num):
+        self.index = 0
+        self.sounds = []
+        self.sound_num = sound_num
+        self.path = path
+        for i in range(self.sound_num):
+            sound = pygame.mixer.Sound(self.path + str(i) + ".mp3")
+            self.sounds.append(sound)
+        self.now_sound = self.sounds[0]
+        self.is_played = False
+
+    def update(self):
+        if(not self.is_played):
+            self.now_sound.stop()
+            self.index += 1
+            self.now_sound = self.sounds[self.index]
+            if self.index >= len(self.sounds):
+                self.is_played = True
+    
+    def play(self):
+        self.now_sound.play()
