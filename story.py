92a25b3185b33853662c69715a41f8c0489592bf
peanutyumyum <jrimit@gmail.com>
2022-10-05 17:54:44 +0900

    fix: button order

--- story.py
+++ story.py
@@ -14,14 +14,14 @@ def process_event(event):
             
     # 마우스 버튼이 스킵 버튼을 눌렸을 때
     if event.type == pygame.MOUSEBUTTONDOWN and event.button == setting.LEFT:
-        if setting.SKIP_X <= event.pos[0] <= setting.SKIP_X + setting.SKIP_W and setting.SKIP_Y <= event.pos[1] <= setting.SKIP_Y + setting.SKIP_H:
+        if setting.SKIP_X <= event.pos[0] <= setting.SKIP_X + setting.SKIP_W and setting.SKIP_Y <= event.pos[1] <= setting.SKIP_Y + setting.SKIP_H and not setting.story_scene_number >= setting.STORY_NUMBERS - 1:
             print("스킵합니다.\n\n")
             setting.skip = True
         elif setting.ALL_SKIP_X <= event.pos[0] <= setting.ALL_SKIP_X + setting.ALL_SKIP_W  and setting.ALL_SKIP_Y <= event.pos[1] <= setting.ALL_SKIP_Y + setting.ALL_SKIP_H:
             print("모두 스킵합니다.\n\n")
             story_sounds.now_sound.stop()
             setting.stage = 2
-        elif setting.BACKWARD_X <= event.pos[0] <= setting.BACKWARD_X + setting.SKIP_W and setting.BACKWARD_Y <= event.pos[1] <= setting.BACKWARD_Y + setting.SKIP_H and not setting.story_scene_number >= setting.STORY_NUMBERS - 1:
+        elif setting.BACKWARD_X <= event.pos[0] <= setting.BACKWARD_X + setting.SKIP_W and setting.BACKWARD_Y <= event.pos[1] <= setting.BACKWARD_Y + setting.SKIP_H:
             setting.backward = True
             print("뒤로 이동합니다.\n\n")
 
@@ -32,7 +32,7 @@ def init_ani():
 
 def init_script():
     global story_script
-    story_script = Script("Play now")
+    story_script = Script("All skip")
 
 def render():
     global story_animation, story_sounds
@@ -72,11 +72,11 @@ def render():
         return 0
     
     setting.screen.blit(story_animation.now_img, (0, 0))
-    setting.screen.blit(setting.skip_img, (setting.SKIP_X, setting.SKIP_Y))
+    setting.screen.blit(setting.backward_img, (setting.BACKWARD_X, setting.BACKWARD_Y))
     setting.screen.blit(setting.all_skip_ing, (setting.ALL_SKIP_X, setting.ALL_SKIP_Y))
     story_script.show_all_script(650, 515)
 
     if not setting.story_scene_number >= setting.STORY_NUMBERS - 1:
-        setting.screen.blit(setting.backward_img, (setting.BACKWARD_X, setting.BACKWARD_Y))
-
-    
+        setting.screen.blit(setting.skip_img, (setting.SKIP_X, setting.SKIP_Y))
+    else:
+        story_script.text = "Play now"
